37655d39287002c2d3cc332c8f781391089ea3b2
mattyocode <matthewoliver@live.co.uk>
2020-07-18 11:57:49 +0100

    Adding validator method

--- bandpassfilter.py
+++ bandpassfilter.py
@@ -1,5 +1,12 @@
-def bandpass(min, max, arr):
-    for i in range(len(arr)):
-        if arr[i] < min:
-            arr[i] = min
-    return arr
+class BandPass():
+
+    def validator(self, arr):
+        return True
+
+    def bandpass(self, min, max, arr):
+        for i in range(len(arr)):
+            if arr[i] < min:
+                arr[i] = min
+            elif arr[i] > max:
+                arr[i] = max
+        return arr
--- test_bandpassfilter.py
+++ test_bandpassfilter.py
@@ -1,11 +1,21 @@
-from bandpassfilter import *
+from bandpassfilter import BandPass
 import pytest
 
+# @pytest.fixture
+# def create_bandpass():
+#     bp = BandPass()
+#     return bp
+
+bp = BandPass()
+
 def test_returns_same_arr_within_limits():
-    assert bandpass(10,20, [10, 20]) == [10,20]
+    assert bp.bandpass(10,20, [10, 20]) == [10,20]
 
 def test_returns_below_min_val_raised_to_min():
-    assert bandpass(10, 20, [9, 20]) == [10, 20]
+    assert bp.bandpass(10, 20, [9, 20]) == [10, 20]
 
 def test_returns_above_max_val_lowered_to_max():
-    assert bandpass(10, 20, [10, 21]) == [10,20]
+    assert bp.bandpass(10, 20, [10, 21]) == [10, 20]
+
+def test_is_integer():
+    assert bp.validator([10, 21]) == True

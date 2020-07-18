00aed9d46642f7670431ac83907a5e599509b99e
mattyocode <matthewoliver@live.co.uk>
2020-07-18 11:32:53 +0100

    Min value raised test passed

--- bandpassfilter.py
+++ bandpassfilter.py
@@ -1,2 +1,5 @@
 def bandpass(min, max, arr):
+    for i in range(len(arr)):
+        if arr[i] < min:
+            arr[i] = min
     return arr
--- test_bandpassfilter.py
+++ test_bandpassfilter.py
@@ -2,5 +2,10 @@ from bandpassfilter import *
 import pytest
 
 def test_returns_same_arr_within_limits():
-    output = bandpass(10,20, [10, 20])
-    assert output == [10,20]
+    assert bandpass(10,20, [10, 20]) == [10,20]
+
+def test_returns_below_min_val_raised_to_min():
+    assert bandpass(10, 20, [9, 20]) == [10, 20]
+
+def test_returns_above_max_val_lowered_to_max():
+    assert bandpass(10, 20, [10, 21]) == [10,20]
